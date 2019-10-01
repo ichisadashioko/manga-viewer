101c4d235f16ab0fdcd500fb3cbcda1c53bd9cff
Ichisada Shioko <yokobang99@gmail.com>
2019-10-01 20:06:09 +0700

    mozilla create catalog application

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""manga_viewer URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
 ]
